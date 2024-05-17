48c1f90f9257d0eaa91ee2427d03ff6d975dc4c8
Harry Lu <innovationharry@gmail.com>
2024-05-16 20:20:45 -0500

    latest update

--- ar-charts/src/App.js
+++ ar-charts/src/App.js
@@ -1,12 +1,20 @@
-import { VRButton, ARButton, XR, Hands, Controllers, useXR, useHitTest, Interactive, RayGrab } from '@react-three/xr'
+import { VRButton, ARButton, XR, Hands, Controllers, useController, useXREvent, useHitTest, Interactive, RayGrab } from '@react-three/xr'
 import { Canvas } from '@react-three/fiber'
 import { extend } from '@react-three/fiber'
 import { Text } from "@react-three/drei"
 import { LineSegments, EdgesGeometry, LineBasicMaterial, BoxGeometry } from 'three';
+import { useFrame } from '@react-three/fiber';
 
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 extend({ Controllers });
 
+
+/*
+创建一个新的bar，这个bar会根据控制器的移动而改变位置
+- 获取控制器坐标 - 完成
+- 获取是哪个控制器进行的交互
+- 创建bar -> 更新bar坐标
+*/
 /*
 when user hover the bar: color: blue -> red
 when user press the button AND hover the bar: generate a new bar and new bar will follow the hand movement, original bar alpha: 1 -> 0.5
@@ -14,33 +22,52 @@ when user release the button: destroy the bar; color: red -> blue; original bar 
 
 */
 function Bar({data, pos}){
+  const leftController = useController('left');
+  const rightController = useController('right');
+
   const [color, setColor] = useState("blue");
   const [opacity, setOpacity] = useState(1);
 
   const [hovered, setHovered] = useState(false);
   const [copy, setCopy] = useState(false);
 
-  const {
-    // An array of connected `XRController`
-    controllers,
-    // Whether the XR device is presenting in an XR session
-    isPresenting,
-    // Whether hand tracking inputs are active
-    isHandTracking,
-    // A THREE.Group representing the XR viewer or player
-    player,
-    // The active `XRSession`
-    session,
-    // `XRSession` foveation. This can be configured as `foveation` on <XR>. Default is `0`
-    foveation,
-    // `XRSession` reference-space type. This can be configured as `referenceSpace` on <XR>. Default is `local-floor`
-    referenceSpace
-  } = useXR();
+  const ref = useRef(null);
+  
+  useXREvent('selectstart', (event) => {
+    console.log("--------SELECT START---------");
+    console.log("selectstart: ", event);
+    console.log("nativeEvent: ", event.nativeEvent);
+    console.log("target: ", event.target);
+    console.log("DATA: ", data);
+    setCopy(true);
+    if (event && event.inputSource) {
+      console.log('Button pressed on controller:', event.inputSource);
+      if (event.target === ref.current) {
+        console.log('Button pressed on controller:', event.inputSource.handedness);
+      }
+    } else {
+      console.error('Event or event.inputSource is undefined:', event);
+    }
+  }, ref);
 
-  console.log("controllers",controllers);
-  console.log("hand tracking", isHandTracking);
+  useXREvent('selectend', (event) => {
+    console.log("--------SELECT END---------");
+    console.log("selectend: ", event);
+    console.log("nativeEvent: ", event.nativeEvent);
+    console.log("target: ", event.target);
+    console.log("DATA: ", data);
+    //setCopy(false);
+    if (event && event.inputSource) {
+      console.log('Button released on controller:', event.inputSource);
+      if (event.target === ref.current) {
+        console.log('Button released on controller:', event.inputSource.handedness);
+      }
+    } else {
+      console.error('Event or event.inputSource is undefined:', event);
+    }
+  }, ref);
 
-  console.log(pos);
+  //console.log("position of bar:",pos);
 
   function handleOnHover(){
     setColor("red"); //blue -> red
@@ -53,13 +80,19 @@ function Bar({data, pos}){
   }
 
   function handleOnSelectStart(){
-    setCopy(true); //create
+    //setCopy(true); //create
     //setOpacity(0.5); //1 -> 0.5
     console.log("clicked");
+    if (leftController) {
+      console.log('Left Controller Position:', leftController.controller.position);
+    }
+    if (rightController) {
+      console.log('Right Controller Position:', rightController.controller.position);
+    }
   }
 
   function handleOnSelectEnd(){
-    setCopy(false); //destroy
+    //setCopy(false); //destroy
     //setOpacity(1); //0.5 -> 1
     //setColor("blue"); //red -> blue
     console.log("quit clicked");
@@ -69,10 +102,8 @@ function Bar({data, pos}){
     <Interactive
       onHover={handleOnHover}
       onBlur={handleOnBlur}
-      onSelectStart={handleOnSelectStart}
-      onSelectEnd={handleOnSelectEnd}
     >
-      <mesh position={pos}>
+      <mesh ref={ref} position={pos}>
         <boxGeometry args={[0.1, data / 4, 0]}/>
         <meshBasicMaterial color={color} opacity={opacity} transparent={true}/>
       </mesh>
@@ -90,8 +121,8 @@ function Bar({data, pos}){
         </>
       }
       {
-        hovered && copy && 
-        <mesh position={pos}>
+        hovered && 
+        <mesh ref={ref} position={pos}>
           <boxGeometry args={[0.1, data / 4, 0]}/>
           <lineSegments>
             <edgesGeometry attach="geometry" args={[new BoxGeometry(0.1, data / 4, 0)]} />
